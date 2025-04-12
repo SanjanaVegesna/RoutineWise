9babf44173d2d25735d5787fc886276b1c9847e5
Deepika <deepikatendulkar1998@gmail.com>
2025-04-11 21:09:34 -0400

    Adding FireBase Login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
+import React from 'react';import Login from './components/Login';
 function App() {
   return (
     <div>
-      <h1>Welcome to RoutineWise</h1>
+      <Login />
     </div>
   );
 }
-
 export default App;
